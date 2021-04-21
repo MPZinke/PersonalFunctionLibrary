fb26bca167b6041b398fc0affe5f7be1259349db
mpzinke <mathew.zinke@hotmail.com>
2021-04-21 17:07:57 -0500

    Updates names to be even shorter

--- Commands/Commands/repos/RemoteList.sh
+++ Commands/Commands/repos/RemoteList.sh
@@ -12,14 +12,14 @@
 ########################################################################################################################
 
 
-declare -A git_repo_dictionary
-git_repo_dictionary["DeskE-PaperDisplay"]="git@github.com:MPZinke/DeskE-PaperDisplay.git"
-git_repo_dictionary["Encrypter"]="git@github.com:MPZinke/Encrypter.git"
-git_repo_dictionary["HueLightsDesktopControl"]="git@github.com:MPZinke/HueLightsDesktopControl.git"
-git_repo_dictionary["LibraryCheckout"]="git@github.com:MPZinke/LibraryCheckout.git"
-git_repo_dictionary["PersonalFunctionLibrary"]="git@github.com:MPZinke/PersonalFunctionLibrary.git"
-git_repo_dictionary["ReadingParser"]="git@github.com:MPZinke/ReadingParser.git"
-git_repo_dictionary["SmartCurtain"]="git@github.com:MPZinke/SmartCurtain.git"
-git_repo_dictionary["SolidEarthFlexing"]="git@github.com:MPZinke/SolidEarthFlexing.git"
-git_repo_dictionary["TheProjectArk"]="git@github.com:MPZinke/TheProjectArk.git"
-git_repo_dictionary["asfd"]="asdf.com"
+declare -A repos_dictionary
+repos_dictionary["DeskE-PaperDisplay"]="git@github.com:MPZinke/DeskE-PaperDisplay.git"
+repos_dictionary["Encrypter"]="git@github.com:MPZinke/Encrypter.git"
+repos_dictionary["HueLightsDesktopControl"]="git@github.com:MPZinke/HueLightsDesktopControl.git"
+repos_dictionary["LibraryCheckout"]="git@github.com:MPZinke/LibraryCheckout.git"
+repos_dictionary["PersonalFunctionLibrary"]="git@github.com:MPZinke/PersonalFunctionLibrary.git"
+repos_dictionary["ReadingParser"]="git@github.com:MPZinke/ReadingParser.git"
+repos_dictionary["SmartCurtain"]="git@github.com:MPZinke/SmartCurtain.git"
+repos_dictionary["SolidEarthFlexing"]="git@github.com:MPZinke/SolidEarthFlexing.git"
+repos_dictionary["TheProjectArk"]="git@github.com:MPZinke/TheProjectArk.git"
+repos_dictionary["asfd"]="asdf.com"
